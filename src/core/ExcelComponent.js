3e674d14e8e9afac51af360de847d64984b4a306
Mohdan <mohdan.barzankaev@gmail.com>
2021-08-03 12:57:50 +0300

    table-logic-complite

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,10 +4,27 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name
+    this.emitter = options.emitter
+    this.unsubs = []
+
+    this.prepare()
+  }
+
+  prepare() {
+
   }
 
   toHTML() {
     return ''
+  }
+
+  $emit(event, ...args) {
+    const unsub = this.emitter.emit(event, ...args)
+    this.unsubs.push(unsub)
+  }
+
+  $on(event, fn) {
+    this.emitter.subscribe(event, fn)
   }
 
   init() {
@@ -16,5 +33,6 @@ export class ExcelComponent extends DomListener {
 
   destroy() {
     this.removeDOMListeners()
+    this.unsubs.forEach(unsub => unsub())
   }
 }
